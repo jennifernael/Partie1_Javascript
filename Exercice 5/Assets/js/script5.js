8c406f9f83ef79412b97b8a890f3a347bcfd1c40
jennifernael <jennifer.nael@hotmail.fr>
2020-11-03 10:45:59 +0100

    ex5

---
+++ Exercice 5/Assets/js/script5.js
@@ -0,0 +1,25 @@
+let x = 4;
+let y = 8;
+
+let a = y / x;
+// a stock 8 / 4 = 2
+
+function sendData(){
+  var number = document.getElementById("name").value;
+}
+
+// Affichage du resultat dans une boîte d'alerte
+alert('Résultat : ' + a);
+
+
+
+
+
+
+
+
+
+
+
+
+
